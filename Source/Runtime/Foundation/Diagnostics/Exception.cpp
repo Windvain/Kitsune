580e77a41b8a5184bd44b77fe30c8f78d85693b9
Windvain <166889703+Windvain@users.noreply.github.com>
2026-03-13 01:54:49 +0700

    Made sure exception names and descriptions are copied, not just referenced.
    When an exception is thrown, the engine will allocate a buffer to store
    the exception's data. If this fails, the thrown exception's data will
    default to "<unknown>" for the exception's name, and "" for the
    exception's description.

--- Source/Runtime/Foundation/Diagnostics/Exception.cpp
+++ Source/Runtime/Foundation/Diagnostics/Exception.cpp
@@ -1,8 +1,31 @@
 #include "Foundation/Diagnostics/Exception.h"
+
 #include "Launch/EngineLoop.h"
+#include "Foundation/String/String.h"
 
 namespace Kitsune
 {
+    namespace Details
+    {
+        // This shouldn't be put in the header file, because it will expose the
+        // BasicString<T> template class.
+        class ExceptionData
+        {
+        public:
+            using AllocatorType = GlobalAllocator;
+
+        public:
+            inline ExceptionData(const char* name, const char* description)
+                : Name(name), Description(description)
+            {
+            }
+
+        public:
+            BasicString<char, AllocatorType> Name;
+            BasicString<char, AllocatorType> Description;
+        };
+    }
+
     Exception::Exception() noexcept
         : Exception("<unknown>", "")
     {
@@ -10,23 +33,40 @@ namespace Kitsune
 
     Exception::Exception(const char* name, const char* description) noexcept
     {
-        m_Name = name;
-        m_Description = description;
+        using Details::ExceptionData;
 
+        void* pointer = Memory::TryAllocate(sizeof(ExceptionData), alignof(ExceptionData));
+        if (pointer == nullptr)
+            return;
+
+        m_Data = static_cast<ExceptionData*>(pointer);
+
+        Memory::ConstructAt(static_cast<ExceptionData*>(m_Data), name, description);
         EngineLoop::GetInstance()->CaptureExceptionBacktrace();
     }
 
     Exception::~Exception() noexcept
     {
+        if (m_Data == nullptr)
+            return;
+
+        Memory::DestroyAt(m_Data);
+        Memory::Free(m_Data, sizeof(Details::ExceptionData));
     }
 
     const char* Exception::GetName() const noexcept
     {
-        return m_Name;
+        if (m_Data == nullptr)
+            return "<unknown>";
+
+        return m_Data->Name.Raw();
     }
 
     const char* Exception::GetDescription() const noexcept
     {
-        return m_Description;
+        if (m_Data == nullptr)
+            return "";
+
+        return m_Data->Description.Raw();
     }
 }
