d5a100b03f3680dca548b8c895ed65228dea6625
Windvain <166889703+Windvain@users.noreply.github.com>
2026-03-14 13:59:09 +0700

    Simplified `IteratorTraits<T>` implementation.
    Forgot to change the implementation after modifying the `Iterator`
    concept.

--- Source/Runtime/Foundation/Iterators/IteratorTraits.h
+++ Source/Runtime/Foundation/Iterators/IteratorTraits.h
@@ -1,48 +1,24 @@
 #pragma once
 
-#include <utility>
+#include "Foundation/Common/Types.h"
 
 namespace Kitsune
 {
-    namespace Details
-    {
-        template<typename It>
-        struct GetDifferenceType_ { /* ... */ };
-
-        template<typename It>
-            requires (requires (It iter1, It iter2) { iter1 - iter2; } &&
-                !requires { typename It::DifferenceType; })
-        struct GetDifferenceType_<It>
-        {
-            using Type = decltype(std::declval<It>() - std::declval<It>());
-        };
-
-        template<typename It>
-            requires requires { typename It::DifferenceType; }
-        struct GetDifferenceType_<It>
-        {
-            using Type = typename It::DifferenceType;
-        };
-
-        template<typename It>
-        struct GetValueType_ { /* ... */ };
-
-        template<typename T>
-        struct GetValueType_<T*> { using Type = T; };
-
-        template<typename It>
-            requires requires { typename It::ValueType; }
-        struct GetValueType_<It>
-        {
-            using Type = typename It::ValueType;
-        };
-    }
-
-    template<typename It>
+    // Gives a uniform interface to the properties of an iterator.
+    // This class exists because a pointer is also an iterator.
+    template<typename Iter>
     class IteratorTraits
     {
     public:
-        using ValueType = Details::GetValueType_<It>::Type;
-        using DifferenceType = Details::GetDifferenceType_<It>::Type;
+        using ValueType = typename Iter::ValueType;
+        using DifferenceType = typename Iter::DifferenceType;
+    };
+
+    template<typename T>
+    class IteratorTraits<T*>
+    {
+    public:
+        using ValueType = T;
+        using DifferenceType = Ptrdiff;
     };
 }
